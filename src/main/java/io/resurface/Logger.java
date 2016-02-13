f8c42eb0cf838878e1413d593e90939a0f25511e
Rob Dickinson <robfromboulder@gmail.com>
2016-02-13 00:40:37 -0700

    Add status logging

--- src/main/java/io/resurface/Logger.java
+++ src/main/java/io/resurface/Logger.java
@@ -2,13 +2,25 @@
 
 package io.resurface;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Properties;
 
 /**
  * Java library for usage logging.
  */
 public class Logger {
+
+    /**
+     * Default destination for log messages.
+     */
+    public static String getDefaultURL() {
+        return "https://resurfaceio.herokuapp.com/messages";
+    }
+
     /**
      * Returns version number from generated properties file.
      */
@@ -21,4 +33,57 @@ public class Logger {
             throw new RuntimeException("Version could not be loaded: " + e.getMessage());
         }
     }
+
+    /**
+     * Default constructor.
+     */
+    public Logger() {
+        this.url = getDefaultURL();
+    }
+
+    /**
+     * Constructor with config params.
+     */
+    public Logger(String url) {
+        this.url = url;
+    }
+
+    private final String version = getVersion();
+    private final String url;
+
+    /**
+     * Formats status message.
+     */
+    public String formatStatus() {
+        return "{\"type\":\"STATUS\",\"source\":\"resurfaceio-logger-java\",\"version\":\"" + version + "\",\"now\":\"" + System.currentTimeMillis() + "\"}";
+    }
+
+    /**
+     * Posts status message.
+     */
+    public boolean logStatus() {
+        return post(formatStatus()) == 200;
+    }
+
+    /**
+     * Perform POST to url.
+     */
+    private int post(String body) {
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setConnectTimeout(5000);
+            con.setReadTimeout(1000);
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            try (OutputStream os = con.getOutputStream()) {
+                os.write(body.getBytes());
+                os.flush();
+            }
+            return con.getResponseCode();
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
 }
--- src/test/java/io/resurface/LoggerTest.java
+++ src/test/java/io/resurface/LoggerTest.java
@@ -4,17 +4,50 @@ package io.resurface;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Tests against Java library for usage logging.
  */
 public class LoggerTest {
+
     @Test
-    public void hasVersionNumber() {
-        String version = Logger.getVersion();
-        assertTrue("version null check", version != null);
-        assertTrue("version length check", version.length() > 0);
-        assertTrue("version startsWith check", version.startsWith("1.0."));
+    public void getDefaultURL() {
+        String url = Logger.getDefaultURL();
+        assertTrue("length check", url.length() > 0);
+        assertTrue("startsWith check", url.startsWith("https://"));
+        assertTrue("single quote check", !url.contains("'"));
+        assertTrue("double quote check", !url.contains("\""));
     }
+
+    @Test
+    public void getVersionNumber() {
+        String version = Logger.getVersion();
+        assertTrue("null check", version != null);
+        assertTrue("length check", version.length() > 0);
+        assertTrue("startsWith check", version.startsWith("1.0."));
+        assertTrue("single quote check", !version.contains("'"));
+        assertTrue("double quote check", !version.contains("\""));
+    }
+
+    @Test
+    public void formatStatus() {
+        String status = new Logger().formatStatus();
+        assertTrue("has type", status.contains("\"type\":\"STATUS\""));
+        assertTrue("has source", status.contains("\"source\":\"resurfaceio-logger-java\""));
+        assertTrue("has version", status.contains("\"version\":\"" + Logger.getVersion() + "\""));
+        assertTrue("has now", status.contains("\"now\":\""));
+    }
+
+    @Test
+    public void logStatus() {
+        assertEquals(true, new Logger().logStatus());
+    }
+
+    @Test
+    public void logStatusToInvalidURL() {
+        assertEquals(false, new Logger(Logger.getDefaultURL() + "/noway3is5this1valid2").logStatus());
+    }
+
 }
