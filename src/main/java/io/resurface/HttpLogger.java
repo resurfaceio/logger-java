a801a0165f0dc9e458c8d1dc642304c0c4026972
Rob Dickinson <robfromboulder@gmail.com>
2016-04-21 23:04:28 -0600

    #4 Refactor out UsageLogger base class

--- src/main/java/io/resurface/HttpLogger.java
+++ src/main/java/io/resurface/HttpLogger.java
@@ -4,80 +4,33 @@ package io.resurface;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
 
 import static io.resurface.JsonMessage.*;
 
 /**
  * Logger for HTTP usage.
  */
-public class HttpLogger {
-
-    /**
-     * Source name for log messages.
-     */
-    public static final String SOURCE = "resurfaceio-logger-java";
-
-    /**
-     * URL destination for log messages unless overridden.
-     */
-    public static final String URL = "https://resurfaceio.herokuapp.com/messages";
+public class HttpLogger extends UsageLogger<HttpLogger> {
 
     /**
      * Initialize enabled logger using default url.
      */
     public HttpLogger() {
-        this.enabled = true;
-        this.url = URL;
-        this.version = version_lookup();
+        super();
     }
 
     /**
      * Initialize enabled logger using custom url.
      */
     public HttpLogger(String url) {
-        this.enabled = true;
-        this.url = url;
-        this.version = version_lookup();
+        super(url);
     }
 
     /**
      * Initialize enabled or disabled logger using custom url.
      */
     public HttpLogger(String url, boolean enabled) {
-        this.enabled = enabled;
-        this.url = url;
-        this.version = version_lookup();
-    }
-
-    private boolean enabled;
-    private boolean tracing = false;
-    private final List<String> tracing_history = new ArrayList<>();
-    private final String url;
-    private final String version;
-
-    /**
-     * Disable this logger.
-     */
-    public HttpLogger disable() {
-        enabled = false;
-        return this;
-    }
-
-    /**
-     * Enable this logger.
-     */
-    public HttpLogger enable() {
-        enabled = true;
-        return this;
+        super(url, enabled);
     }
 
     /**
@@ -108,20 +61,6 @@ public class HttpLogger {
             append(json, "body", body);
         }
         return finish(json);
-    }
-
-    /**
-     * Returns true if this logger is enabled.
-     */
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    /**
-     * Returns true if keeping a copy of all posted messages.
-     */
-    public boolean isTracing() {
-        return tracing;
     }
 
     /**
@@ -160,84 +99,6 @@ public class HttpLogger {
             return post(json.toString()) == 200;
         } else {
             return true;
-        }
-    }
-
-    /**
-     * Logs message (via HTTP post) to remote url.
-     */
-    public int post(String json) {
-        if (tracing) {
-            tracing_history.add(json);
-            return 200;
-        } else {
-            try {
-                URL url = new URL(this.url);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setConnectTimeout(5000);
-                con.setReadTimeout(1000);
-                con.setRequestMethod("POST");
-                con.setDoOutput(true);
-                try (OutputStream os = con.getOutputStream()) {
-                    os.write(json.getBytes());
-                    os.flush();
-                }
-                return con.getResponseCode();
-            } catch (IOException ioe) {
-                return 404;
-            }
-        }
-    }
-
-    /**
-     * Returns unmodifiable copy of recently posted messages.
-     */
-    public List<String> tracingHistory() {
-        return Collections.unmodifiableList(tracing_history);
-    }
-
-    /**
-     * Starts keeping copy of all posted messages.
-     */
-    public HttpLogger tracingStart() {
-        tracing = true;
-        tracing_history.clear();
-        return this;
-    }
-
-    /**
-     * Stops tracing and clears current tracing history.
-     */
-    public HttpLogger tracingStop() {
-        tracing = false;
-        tracing_history.clear();
-        return this;
-    }
-
-    /**
-     * Returns url destination where messages are sent.
-     */
-    public String url() {
-        return url;
-    }
-
-    /**
-     * Returns cached version number.
-     */
-    public String version() {
-        return version;
-    }
-
-    /**
-     * Retrieves version number from runtime properties file.
-     */
-    public static String version_lookup() {
-        try (InputStream is = HttpLogger.class.getResourceAsStream("/version.properties")) {
-            Properties p = new Properties();
-            p.load(is);
-            return p.getProperty("version", null);
-        } catch (Exception e) {
-            throw new RuntimeException("Version could not be loaded: " + e.getMessage());
         }
     }
 
---
+++ src/main/java/io/resurface/UsageLogger.java
@@ -0,0 +1,173 @@
+// Copyright (c) 2016 Resurface Labs LLC, All Rights Reserved
+
+package io.resurface;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Abstract base class for all usage loggers, with type-safe chaining methods.
+ */
+public abstract class UsageLogger<T extends UsageLogger> {
+
+    /**
+     * Source name for log messages.
+     */
+    public static final String SOURCE = "resurfaceio-logger-java";
+
+    /**
+     * URL destination for log messages unless overridden.
+     */
+    public static final String URL = "https://resurfaceio.herokuapp.com/messages";
+
+    /**
+     * Initialize enabled logger using default url.
+     */
+    public UsageLogger() {
+        this.enabled = true;
+        this.url = URL;
+        this.version = version_lookup();
+    }
+
+    /**
+     * Initialize enabled logger using custom url.
+     */
+    public UsageLogger(String url) {
+        this.enabled = true;
+        this.url = url;
+        this.version = version_lookup();
+    }
+
+    /**
+     * Initialize enabled or disabled logger using custom url.
+     */
+    public UsageLogger(String url, boolean enabled) {
+        this.enabled = enabled;
+        this.url = url;
+        this.version = version_lookup();
+    }
+
+    protected boolean enabled;
+    protected boolean tracing = false;
+    protected final List<String> tracing_history = new ArrayList<>();
+    protected final String url;
+    protected final String version;
+
+    /**
+     * Disable this logger.
+     */
+    public T disable() {
+        enabled = false;
+        return (T) this;
+    }
+
+    /**
+     * Enable this logger.
+     */
+    public T enable() {
+        enabled = true;
+        return (T) this;
+    }
+
+    /**
+     * Returns true if this logger is enabled.
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Returns true if keeping a copy of all posted messages.
+     */
+    public boolean isTracing() {
+        return tracing;
+    }
+
+    /**
+     * Returns unmodifiable copy of recently posted messages.
+     */
+    public List<String> tracingHistory() {
+        return Collections.unmodifiableList(tracing_history);
+    }
+
+    /**
+     * Starts keeping copy of all posted messages.
+     */
+    public T tracingStart() {
+        tracing = true;
+        tracing_history.clear();
+        return (T) this;
+    }
+
+    /**
+     * Stops tracing and clears current tracing history.
+     */
+    public T tracingStop() {
+        tracing = false;
+        tracing_history.clear();
+        return (T) this;
+    }
+
+    /**
+     * Returns url destination where messages are sent.
+     */
+    public String url() {
+        return url;
+    }
+
+    /**
+     * Returns cached version number.
+     */
+    public String version() {
+        return version;
+    }
+
+    /**
+     * Retrieves version number from runtime properties file.
+     */
+    public static String version_lookup() {
+        try (InputStream is = HttpLogger.class.getResourceAsStream("/version.properties")) {
+            Properties p = new Properties();
+            p.load(is);
+            return p.getProperty("version", null);
+        } catch (Exception e) {
+            throw new RuntimeException("Version could not be loaded: " + e.getMessage());
+        }
+    }
+
+    // TEMPORARY, THIS WILL BE CHANGING TO PROTECTED
+
+    /**
+     * Logs message (via HTTP post) to remote url.
+     */
+    public int post(String json) {
+        if (tracing) {
+            tracing_history.add(json);
+            return 200;
+        } else {
+            try {
+                URL url = new URL(this.url);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setConnectTimeout(5000);
+                con.setReadTimeout(1000);
+                con.setRequestMethod("POST");
+                con.setDoOutput(true);
+                try (OutputStream os = con.getOutputStream()) {
+                    os.write(json.getBytes());
+                    os.flush();
+                }
+                return con.getResponseCode();
+            } catch (IOException ioe) {
+                return 404;
+            }
+        }
+    }
+
+}
