929de3f80de0a5217718192dd9f0fada9ca15851
Rob Dickinson <robfromboulder@gmail.com>
2016-04-22 22:32:22 -0600

    #5 Optimize performance for servlet logger

--- src/main/java/io/resurface/HttpLoggerForServlets.java
+++ src/main/java/io/resurface/HttpLoggerForServlets.java
@@ -17,38 +17,44 @@ public class HttpLoggerForServlets implements Filter {
      */
     public void init(FilterConfig config) {
         this.config = config;
+        this.logger = HttpLoggerFactory.get();
     }
-
-    private FilterConfig config;
 
     /**
      * Called when filter is taken out of service.
      */
     public void destroy() {
         this.config = null;
+        this.logger = null;
     }
+
+    private FilterConfig config;
+    private HttpLogger logger;
 
     /**
      * Called when request/response passes through the filter chain.
      */
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-        LoggedResponseWrapper wrapper = new LoggedResponseWrapper(response);
-        chain.doFilter(request, wrapper);
-        if (response.getStatus() != 304) {
-            String content_type = response.getContentType();
-            String encoding = response.getCharacterEncoding();
-            if ((content_type != null) && (encoding != null)) {
-                boolean is_html = content_type.startsWith("text/html");
-                boolean is_json = content_type.startsWith("application/json");
-                boolean is_soap = content_type.startsWith("application/soap+xml");
-                boolean is_xml1 = content_type.startsWith("application/xml");
-                boolean is_xml2 = content_type.startsWith("text/xml");
-                if (is_html || is_json || is_soap || is_xml1 || is_xml2) {
-                    HttpLogger logger = HttpLoggerFactory.get();
-                    logger.logRequest(request);
-                    logger.logResponse(response, new String(wrapper.read(), encoding));
+        if (!logger.isActive()) {
+            chain.doFilter(req, res);
+        } else {
+            HttpServletRequest request = (HttpServletRequest) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+            LoggedResponseWrapper wrapper = new LoggedResponseWrapper(response);
+            chain.doFilter(request, wrapper);
+            if (response.getStatus() != 304) {
+                String content_type = response.getContentType();
+                String encoding = response.getCharacterEncoding();
+                if ((content_type != null) && (encoding != null)) {
+                    boolean is_html = content_type.startsWith("text/html");
+                    boolean is_json = content_type.startsWith("application/json");
+                    boolean is_soap = content_type.startsWith("application/soap+xml");
+                    boolean is_xml1 = content_type.startsWith("application/xml");
+                    boolean is_xml2 = content_type.startsWith("text/xml");
+                    if (is_html || is_json || is_soap || is_xml1 || is_xml2) {
+                        logger.logRequest(request);
+                        logger.logResponse(response, new String(wrapper.read(), encoding));
+                    }
                 }
             }
         }
