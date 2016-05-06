e01d1535e9a223d3c1a2ff32670cca6ff897ac50
Rob Dickinson <robfromboulder@gmail.com>
2016-05-06 00:07:50 -0600

    #8 Add missing test coverage

--- src/test/java/io/resurface/tests/HttpServletRequestImplTest.java
+++ src/test/java/io/resurface/tests/HttpServletRequestImplTest.java
@@ -5,12 +5,32 @@ package io.resurface.tests;
 import io.resurface.HttpServletRequestImpl;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
 import static org.junit.Assert.assertTrue;
 
 /**
  * Tests against HttpServletRequest implementation for custom usage logging.
  */
 public class HttpServletRequestImplTest {
+
+    @Test
+    public void useCharacterEncoding() throws UnsupportedEncodingException {
+        String val = "UTF-8";
+        HttpServletRequestImpl impl = new HttpServletRequestImpl();
+        assertTrue("null by default", impl.getCharacterEncoding() == null);
+        impl.setCharacterEncoding(val);
+        assertTrue("value set ok", impl.getCharacterEncoding().equals(val));
+    }
+
+    @Test
+    public void useContentType() {
+        String val = "text/html";
+        HttpServletRequestImpl impl = new HttpServletRequestImpl();
+        assertTrue("null by default", impl.getContentType() == null);
+        impl.setContentType(val);
+        assertTrue("value set ok", impl.getContentType().equals(val));
+    }
 
     @Test
     public void useRequestURL() {
