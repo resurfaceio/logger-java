0a514c5e2281416df4c59e8dc32e3bee8a62cf55
Rob Dickinson <robfromboulder@gmail.com>
2017-10-07 23:49:12 -0600

    #41 Add test case for string content type detection

--- src/test/java/io/resurface/tests/HttpLoggerTest.java
+++ src/test/java/io/resurface/tests/HttpLoggerTest.java
@@ -53,6 +53,27 @@ public class HttpLoggerTest {
     }
 
     @Test
+    public void detectsStringContentTypesTest() {
+        HttpLogger logger = new HttpLogger();
+
+        expect(logger.isStringContentType("application/json")).toBeTrue();
+        expect(HttpLogger.isStringContentType("application/soap")).toBeTrue();
+        expect(HttpLogger.isStringContentType("application/xml")).toBeTrue();
+        expect(HttpLogger.isStringContentType("application/x-www-form-urlencoded")).toBeTrue();
+        expect(HttpLogger.isStringContentType("text/html")).toBeTrue();
+        expect(HttpLogger.isStringContentType("text/html; charset=utf-8")).toBeTrue();
+        expect(HttpLogger.isStringContentType("text/plain")).toBeTrue();
+        expect(HttpLogger.isStringContentType("text/plain123")).toBeTrue();
+        expect(HttpLogger.isStringContentType("text/xml")).toBeTrue();
+        expect(HttpLogger.isStringContentType("Text/XML")).toBeTrue();
+
+        expect(logger.isStringContentType("application/")).toBeFalse();
+        expect(HttpLogger.isStringContentType("json")).toBeFalse();
+        expect(HttpLogger.isStringContentType("html")).toBeFalse();
+        expect(HttpLogger.isStringContentType("xml")).toBeFalse();
+    }
+
+    @Test
     public void hasValidAgentTest() {
         String agent = HttpLogger.AGENT;
         expect(agent.length()).toBeGreaterThan(0);
