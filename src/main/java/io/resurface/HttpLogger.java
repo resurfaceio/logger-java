a47932164a74456334651009873163b441a205dd
Rob Dickinson <robfromboulder@gmail.com>
2017-12-20 00:03:58 -0700

    #50 Extract HttpMessageImpl

--- src/main/java/io/resurface/HttpLogger.java
+++ src/main/java/io/resurface/HttpLogger.java
@@ -4,9 +4,6 @@ package io.resurface;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -88,16 +85,7 @@ public class HttpLogger extends BaseLogger<HttpLogger> {
      */
     public String format(HttpServletRequest request, HttpServletResponse response,
                          String response_body, String request_body, long now) {
-        List<String[]> message = new ArrayList<>();
-        message.add(new String[]{"request_method", request.getMethod()});
-        String formatted_url = formatURL(request);
-        if (formatted_url != null) message.add(new String[]{"request_url", formatted_url});
-        message.add(new String[]{"response_code", String.valueOf(response.getStatus())});
-        appendRequestHeaders(message, request);
-        appendRequestParams(message, request);
-        appendResponseHeaders(message, response);
-        if (request_body != null && !request_body.equals("")) message.add(new String[]{"request_body", request_body});
-        if (response_body != null && !response_body.equals("")) message.add(new String[]{"response_body", response_body});
+        List<String[]> message = HttpMessageImpl.build(request, response, response_body, request_body);
         message.add(new String[]{"agent", this.agent});
         message.add(new String[]{"version", this.version});
         message.add(new String[]{"now", String.valueOf(now)});
@@ -130,57 +118,6 @@ public class HttpLogger extends BaseLogger<HttpLogger> {
      */
     public boolean log(HttpServletRequest request, HttpServletResponse response, String response_body, String request_body) {
         return !isEnabled() || submit(format(request, response, response_body, request_body));
-    }
-
-    /**
-     * Adds request headers to message.
-     */
-    protected void appendRequestHeaders(List<String[]> message, HttpServletRequest request) {
-        Enumeration<String> header_names = request.getHeaderNames();
-        while (header_names.hasMoreElements()) {
-            String name = header_names.nextElement();
-            Enumeration<String> e = request.getHeaders(name);
-            name = "request_header:" + name.toLowerCase();
-            while (e.hasMoreElements()) message.add(new String[]{name, e.nextElement()});
-        }
-    }
-
-    /**
-     * Adds request params to message.
-     */
-    protected void appendRequestParams(List<String[]> message, HttpServletRequest request) {
-        Enumeration<String> param_names = request.getParameterNames();
-        while (param_names.hasMoreElements()) {
-            String name = param_names.nextElement();
-            String[] values = request.getParameterValues(name);
-            if (values != null) {
-                name = "request_param:" + name.toLowerCase();
-                for (String value : values) message.add(new String[]{name, value});
-            }
-        }
-    }
-
-    /**
-     * Adds response headers to message.
-     */
-    protected void appendResponseHeaders(List<String[]> message, HttpServletResponse response) {
-        for (String name : response.getHeaderNames()) {
-            Iterator<String> i = response.getHeaders(name).iterator();
-            name = "response_header:" + name.toLowerCase();
-            while (i.hasNext()) message.add(new String[]{name, i.next()});
-        }
-    }
-
-    /**
-     * Returns complete request URL including query string.
-     */
-    protected String formatURL(HttpServletRequest request) {
-        StringBuffer url = request.getRequestURL();
-        if (url != null) {
-            String queryString = request.getQueryString();
-            if (queryString != null) url.append('?').append(queryString);
-        }
-        return (url == null) ? null : url.toString();
     }
 
     private static final String STRING_TYPES = "(?i)^text/(html|plain|xml)|application/(json|soap|xml|x-www-form-urlencoded)";
---
+++ src/main/java/io/resurface/HttpMessageImpl.java
@@ -0,0 +1,86 @@
+// © 2016-2017 Resurface Labs LLC
+
+package io.resurface;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Message implementation for HTTP/HTTPS protocol.
+ */
+public class HttpMessageImpl {
+
+    /**
+     * Builds list of key/value pairs for HTTP request and response.
+     */
+    public static List<String[]> build(HttpServletRequest request, HttpServletResponse response,
+                                       String response_body, String request_body) {
+        List<String[]> message = new ArrayList<>();
+        message.add(new String[]{"request_method", request.getMethod()});
+        String formatted_url = formatURL(request);
+        if (formatted_url != null) message.add(new String[]{"request_url", formatted_url});
+        message.add(new String[]{"response_code", String.valueOf(response.getStatus())});
+        appendRequestHeaders(message, request);
+        appendRequestParams(message, request);
+        appendResponseHeaders(message, response);
+        if (request_body != null && !request_body.equals("")) message.add(new String[]{"request_body", request_body});
+        if (response_body != null && !response_body.equals("")) message.add(new String[]{"response_body", response_body});
+        return message;
+    }
+
+    /**
+     * Adds request headers to message.
+     */
+    private static void appendRequestHeaders(List<String[]> message, HttpServletRequest request) {
+        Enumeration<String> header_names = request.getHeaderNames();
+        while (header_names.hasMoreElements()) {
+            String name = header_names.nextElement();
+            Enumeration<String> e = request.getHeaders(name);
+            name = "request_header:" + name.toLowerCase();
+            while (e.hasMoreElements()) message.add(new String[]{name, e.nextElement()});
+        }
+    }
+
+    /**
+     * Adds request params to message.
+     */
+    private static void appendRequestParams(List<String[]> message, HttpServletRequest request) {
+        Enumeration<String> param_names = request.getParameterNames();
+        while (param_names.hasMoreElements()) {
+            String name = param_names.nextElement();
+            String[] values = request.getParameterValues(name);
+            if (values != null) {
+                name = "request_param:" + name.toLowerCase();
+                for (String value : values) message.add(new String[]{name, value});
+            }
+        }
+    }
+
+    /**
+     * Adds response headers to message.
+     */
+    private static void appendResponseHeaders(List<String[]> message, HttpServletResponse response) {
+        for (String name : response.getHeaderNames()) {
+            Iterator<String> i = response.getHeaders(name).iterator();
+            name = "response_header:" + name.toLowerCase();
+            while (i.hasNext()) message.add(new String[]{name, i.next()});
+        }
+    }
+
+    /**
+     * Returns complete request URL including query string.
+     */
+    private static String formatURL(HttpServletRequest request) {
+        StringBuffer url = request.getRequestURL();
+        if (url != null) {
+            String queryString = request.getQueryString();
+            if (queryString != null) url.append('?').append(queryString);
+        }
+        return (url == null) ? null : url.toString();
+    }
+
+}
