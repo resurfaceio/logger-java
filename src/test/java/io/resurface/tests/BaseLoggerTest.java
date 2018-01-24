1da459a56ceb2068e73b99472dea2069d1863234
Rob Dickinson <robfromboulder@gmail.com>
2018-01-23 21:14:21 -0700

    Update version to 1.9.0-SNAPSHOT

--- src/test/java/io/resurface/tests/BaseLoggerTest.java
+++ src/test/java/io/resurface/tests/BaseLoggerTest.java
@@ -68,7 +68,7 @@ public class BaseLoggerTest {
         String version = BaseLogger.version_lookup();
         expect(version).toBeNotNull();
         expect(version.length()).toBeGreaterThan(0);
-        expect(version).toStartWith("1.8.");
+        expect(version).toStartWith("1.9.");
         expect(version.contains("\\")).toBeFalse();
         expect(version.contains("\"")).toBeFalse();
         expect(version.contains("'")).toBeFalse();
