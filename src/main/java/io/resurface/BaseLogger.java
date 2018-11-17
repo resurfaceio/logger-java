7483447158c30b054d7060a73bb7c0193229b2a5
Rob Dickinson <robfromboulder@gmail.com>
2018-11-16 23:46:40 -0700

    Update version to 1.9.2

--- src/main/java/io/resurface/BaseLogger.java
+++ src/main/java/io/resurface/BaseLogger.java
@@ -205,7 +205,7 @@ public class BaseLogger<T extends BaseLogger> {
      * Returns version number for this logger.
      */
     public static String version_lookup() {
-        return "1.9.2-SNAPSHOT";
+        return "1.9.2";
     }
 
     protected final String agent;
