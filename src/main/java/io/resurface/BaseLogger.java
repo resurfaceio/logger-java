e8bfb17c0d669505b37cf5f12898e07495594328
Rob Dickinson <robfromboulder@gmail.com>
2019-06-03 12:29:18 -0700

    #57 Fix version number

--- src/main/java/io/resurface/BaseLogger.java
+++ src/main/java/io/resurface/BaseLogger.java
@@ -205,7 +205,7 @@ public class BaseLogger<T extends BaseLogger> {
      * Returns version number for this logger.
      */
     public static String version_lookup() {
-        return "1.9.3-SNAPSHOT";
+        return "1.9.4-SNAPSHOT";
     }
 
     protected final String agent;
