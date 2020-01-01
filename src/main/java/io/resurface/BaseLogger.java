cb8c98054eed295a57f905f53081cb5773004c4f
Rob Dickinson <robfromboulder@gmail.com>
2020-01-01 13:14:58 -0700

    Update version to 2.0.5-SNAPSHOT

--- src/main/java/io/resurface/BaseLogger.java
+++ src/main/java/io/resurface/BaseLogger.java
@@ -253,7 +253,7 @@ public class BaseLogger<T extends BaseLogger> {
      * Returns version number for this logger.
      */
     public static String version_lookup() {
-        return "2.0.4";
+        return "2.0.5-SNAPSHOT";
     }
 
     protected final String agent;
