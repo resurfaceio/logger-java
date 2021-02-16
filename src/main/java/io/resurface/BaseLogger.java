cb583a84878c42d3a58dda591bd6a4709aaba20d
Rob Dickinson <robfromboulder@gmail.com>
2021-02-16 01:13:48 -0700

    Update version to 2.1.1

--- src/main/java/io/resurface/BaseLogger.java
+++ src/main/java/io/resurface/BaseLogger.java
@@ -257,7 +257,7 @@ public class BaseLogger<T extends BaseLogger> {
      * Returns version number for this logger.
      */
     public static String version_lookup() {
-        return "2.1.1-SNAPSHOT";
+        return "2.1.1";
     }
 
     protected final String agent;
