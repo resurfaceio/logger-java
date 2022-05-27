17253254c3d645ee177a7f188f2c70542491a87f
Ramon Marquez <ramn.marquez@gmail.com>
2022-05-27 15:31:59 -0400

    chore: change default batch size to 100 kiB (51200 chars)

--- src/main/java/io/resurface/Dispatcher.java
+++ src/main/java/io/resurface/Dispatcher.java
@@ -3,26 +3,25 @@ package io.resurface;
 public class Dispatcher extends Thread{
 
     protected Dispatcher(BaseLogger logger) {
-        this(logger, 100);
+        this(logger, 50 * 1024);
     }
 
     protected Dispatcher(BaseLogger logger, int threshold) {
         this.logger = logger;
-        this.thresh = threshold * 2;
-        this.buffer = new StringBuilder(this.thresh);
+        thresh = threshold;
+        buffer = new StringBuilder(thresh);
     }
 
     public void run() {
         try {
             while (true) {
 //                String msg = (String) this.logger.msg_queue.take();
-                if ((this.logger.msg_queue.peek() == null && this.buffer.length() != 0) || this.buffer.length() == thresh) {
-                    String msg = this.buffer.toString();
-                    this.buffer = new StringBuilder();
-                    this.logger.dispatch(msg);
+                if ((logger.msg_queue.peek() == null && buffer.length() != 0) || buffer.length() == thresh) {
+                    String msg = buffer.toString();
+                    buffer = new StringBuilder();
+                    logger.dispatch(msg);
                 }
-                this.buffer.append(this.logger.msg_queue.take());
-                this.buffer.append("\n");
+                buffer.append(logger.msg_queue.take()).append("\n");
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
